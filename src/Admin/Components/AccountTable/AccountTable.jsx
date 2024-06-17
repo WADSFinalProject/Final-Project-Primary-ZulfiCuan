6332be7792aa986ce7f0be1f4ccbbe397d384001
Raihan Zulfi <114371959+raizulfi@users.noreply.github.com>
2024-06-17 22:28:43 +0700

    table looks neeter

--- src/Admin/Components/AccountTable/AccountTable.jsx
+++ src/Admin/Components/AccountTable/AccountTable.jsx
@@ -10,10 +10,7 @@ import TableRow from '@mui/material/TableRow';
 import IconButton from '@mui/material/IconButton';
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
-import Backdrop from '@mui/material/Backdrop';
-import CircularProgress from '@mui/material/CircularProgress';
-import Button from '@mui/material/Button';
-import EditAccountPopup from '../PopupEditAssest/EditAccountPopup'; // Import the EditAccountPopup component
+import EditAccountPopup from '../PopupEditAssest/EditAccountPopup';
 import DeleteAccountPopup from '../PopupEditAssest/DeleteAccountPopup';
 
 const columns = [
@@ -22,7 +19,7 @@ const columns = [
   { id: 'email', label: 'Email', minWidth: 170, align: 'center' },
   { id: 'password', label: 'Password', minWidth: 170, align: 'center' },
   { id: 'birthDate', label: 'Birth Date', minWidth: 170, align: 'center' },
-  { id: 'role', label: 'Role', minWidth: 170, align: 'center' }, // Updated to center align
+  { id: 'role', label: 'Role', minWidth: 170, align: 'center' },
   { id: 'action', label: 'Action', minWidth: 170, align: 'center' },
 ];
 
@@ -31,12 +28,17 @@ const initialRows = [
   { id: 'U109', name: 'Jane Smith', email: 'janesmith@centra.com', password: 'xxxxxxxxx', birthDate: '05/15/1990', role: 'User' },
   { id: 'U110', name: 'Mike Johnson', email: 'mikejohnson@centra.com', password: 'xxxxxxxxx', birthDate: '08/23/1985', role: 'User' },
   { id: 'U111', name: 'Emily Davis', email: 'emilydavis@centra.com', password: 'xxxxxxxxx', birthDate: '11/19/1992', role: 'Admin' },
+  { id: 'U108', name: 'John Doe', email: 'johndoe@centra.com', password: 'xxxxxxxxx', birthDate: '12/04/1987', role: 'Admin' },
+  { id: 'U109', name: 'Jane Smith', email: 'janesmith@centra.com', password: 'xxxxxxxxx', birthDate: '05/15/1990', role: 'User' },
+  { id: 'U110', name: 'Mike Johnson', email: 'mikejohnson@centra.com', password: 'xxxxxxxxx', birthDate: '08/23/1985', role: 'User' },
+  { id: 'U111', name: 'Emily Davis', email: 'emilydavis@centra.com', password: 'xxxxxxxxx', birthDate: '11/19/1992', role: 'Admin' },
+  { id: 'U109', name: 'Jane Smith', email: 'janesmith@centra.com', password: 'xxxxxxxxx', birthDate: '05/15/1990', role: 'User' },
   // ... (rest of the rows)
 ];
 
 export default function AccountTable() {
   const [page, setPage] = useState(0);
-  const rowsPerPage = 10;
+  const rowsPerPage = 8;
   const [editOpen, setEditOpen] = useState(false);
   const [deleteOpen, setDeleteOpen] = useState(false);
   const [selectedAccount, setSelectedAccount] = useState(null);
@@ -64,120 +66,117 @@ export default function AccountTable() {
   };
 
   const handleSaveAccount = (updatedAccount) => {
-    console.log('Updated account details:', updatedAccount); // Debugging line
-    // Update the rows with the new account details
+    console.log('Updated account details:', updatedAccount);
     const updatedRows = rows.map((row) =>
       row.id === updatedAccount.id ? updatedAccount : row
     );
     setRows(updatedRows);
-    handleEditClose(); // Close the edit popup
+    handleEditClose();
   };
 
   const emptyRows = rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
 
   return (
-  <Paper sx={{ width: '100%', overflow: 'hidden', borderRadius: '16px', maxWidth: '100%' }}>
-    <TableContainer sx={{ maxHeight: 'calc(100vh - 150px)', overflowX: 'auto' }}>
-      <Table stickyHeader aria-label="sticky table">
-        <TableHead>
-          <TableRow>
-            {columns.map((column) => (
-              <TableCell
-                key={column.id}
-                align={column.align}
-                sx={{ minWidth: column.minWidth, backgroundColor: '#002855', color: 'white', textAlign: 'center' }}
-              >
-                {column.label}
-              </TableCell>
+    <Paper sx={{ width: '100%', overflow: 'hidden', borderRadius: '16px', maxWidth: '100%' }}>
+      <TableContainer sx={{ maxHeight: 'calc(100vh - 150px)', overflowX: 'auto' }}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              {columns.map((column) => (
+                <TableCell
+                  key={column.id}
+                  align={column.align}
+                  sx={{ minWidth: column.minWidth, backgroundColor: '#002855', color: 'white', textAlign: 'center' }}
+                >
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody sx={{ backgroundColor: '#ebebeb' }}>
+            {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row, rowIndex) => (
+              <TableRow hover role="checkbox" tabIndex={-1} key={rowIndex}>
+                {columns.map((column) => {
+                  const value = row[column.id];
+                  return (
+                    <TableCell
+                      key={column.id}
+                      align={column.align}
+                      sx={{
+                        color: '#002855',
+                        fontWeight: 500,
+                        fontFamily: 'var(--font-inter)',
+                        borderBottom: '1px solid #04315b',
+                        textAlign: column.align,
+                      }}
+                    >
+                      {column.id === 'action' ? (
+                        <>
+                          <IconButton
+                            aria-label="edit"
+                            sx={{
+                              color: '#ffffff',
+                              backgroundColor: '#ff7c52',
+                              borderRadius: '8px',
+                              marginRight: '5px',
+                              '&:hover': {
+                                backgroundColor: '#ff6a3e',
+                              },
+                            }}
+                            onClick={() => handleEditOpen(row)}
+                          >
+                            <EditOutlinedIcon />
+                          </IconButton>
+                          <IconButton
+                            aria-label="delete"
+                            sx={{
+                              color: '#ffffff',
+                              backgroundColor: '#ff7c52',
+                              borderRadius: '8px',
+                              '&:hover': {
+                                backgroundColor: '#ff6a3e',
+                              },
+                            }}
+                            onClick={handleDeleteOpen}
+                          >
+                            <DeleteOutlineIcon />
+                          </IconButton>
+                        </>
+                      ) : (
+                        value
+                      )}
+                    </TableCell>
+                  );
+                })}
+              </TableRow>
             ))}
-          </TableRow>
-        </TableHead>
-        <TableBody sx={{ backgroundColor: '#ebebeb' }}>
-          {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row, rowIndex) => (
-            <TableRow hover role="checkbox" tabIndex={-1} key={rowIndex}>
-              {columns.map((column) => {
-                const value = row[column.id];
-                return (
-                  <TableCell
-                    key={column.id}
-                    align={column.align}
-                    sx={{
-                      color: '#002855',
-                      fontWeight: 500,
-                      fontFamily: 'var(--font-inter)',
-                      borderBottom: '1px solid #04315b',
-                      textAlign: column.align,
-                    }}
-                  >
-                    {column.id === 'action' ? (
-                      <>
-                        <IconButton
-                          aria-label="edit"
-                          sx={{
-                            color: '#ffffff',
-                            backgroundColor: '#ff7c52',
-                            borderRadius: '8px',
-                            marginRight: '5px',
-                            '&:hover': {
-                              backgroundColor: '#ff6a3e',
-                            },
-                          }}
-                          onClick={() => handleEditOpen(row)}
-                        >
-                          <EditOutlinedIcon />
-                        </IconButton>
-                        <IconButton
-                          aria-label="delete"
-                          sx={{
-                            color: '#ffffff',
-                            backgroundColor: '#ff7c52',
-                            borderRadius: '8px',
-                            '&:hover': {
-                              backgroundColor: '#ff6a3e',
-                            },
-                          }}
-                          onClick={handleDeleteOpen}
-                        >
-                          <DeleteOutlineIcon />
-                        </IconButton>
-                      </>
-                    ) : (
-                      value
-                    )}
-                  </TableCell>
-                );
-              })}
-            </TableRow>
-          ))}
-          {emptyRows > 0 && (
-            <TableRow style={{ height: 53 * emptyRows }}>
-              <TableCell colSpan={columns.length} />
-            </TableRow>
-          )}
-        </TableBody>
-      </Table>
-    </TableContainer>
-    <TablePagination
-      sx={{ backgroundColor: '#ebebeb' }}
-      rowsPerPageOptions={[10]}
-      component="div"
-      count={rows.length}
-      rowsPerPage={rowsPerPage}
-      page={page}
-      onPageChange={handleChangePage}
-    />
-    <EditAccountPopup
-      open={editOpen}
-      onClose={handleEditClose}
-      accountDetails={selectedAccount}
-      onSave={handleSaveAccount}
-    />
-    <DeleteAccountPopup
-      open={deleteOpen}
-      onClose={handleDeleteClose}
-      // onDelete={handleDeleteAccount}
-    />
-  </Paper>
-
+            {emptyRows > 0 && (
+              <TableRow style={{ height: 53 * emptyRows }}>
+                <TableCell colSpan={columns.length} />
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        sx={{ backgroundColor: '#ebebeb' }}
+        rowsPerPageOptions={[10]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+      />
+      <EditAccountPopup
+        open={editOpen}
+        onClose={handleEditClose}
+        accountDetails={selectedAccount}
+        onSave={handleSaveAccount}
+      />
+      <DeleteAccountPopup
+        open={deleteOpen}
+        onClose={handleDeleteClose}
+      />
+    </Paper>
   );
 }

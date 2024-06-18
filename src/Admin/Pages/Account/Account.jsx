a244df94f69103d5009f57b033b0dc9d2bfc1fdb
Raihan Zulfi <114371959+raizulfi@users.noreply.github.com>
2024-06-18 07:57:28 +0700

    lol

--- src/Admin/Pages/Account/Account.jsx
+++ src/Admin/Pages/Account/Account.jsx
@@ -21,7 +21,7 @@ function Account({togglePage, pages}) {
     <div className="accountPage-admin">
       <SideBar togglePage={togglePage}/>
       <div className="accountContent-admin">
-        <Navbar togglePage={togg`lePage}/>
+        <Navbar togglePage={togglePage}/>
         {showAddAccount ? (
           <div className="editAccountContainer-admin">
             <AddAccountPage onBack={handleBack} />
